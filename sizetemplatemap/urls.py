78c7b02a827362672974555631560e3fa4aaadb3
Amy Cerrito <acerrito@yahoo.com>
2012-12-14 19:20:06 -0500

    add url

--- sizetemplatemap/urls.py
+++ sizetemplatemap/urls.py
@@ -6,4 +6,5 @@ urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^index\.html', views.index, name='index'),
     url(r'^(?P<creative_size_id>\d+)/$', views.detail, name='detail'),
+    url(r'^(?P<creative_size_id>\d+)/edit/$', views.cs_edit, name='edit'),
 )
