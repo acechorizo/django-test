0490a3bfa95e4511596c3add65b7772ec7e5c230
Amy Cerrito <acerrito@yahoo.com>
2012-12-20 17:58:47 -0500

    update logic

--- sizetemplatemap/views.py
+++ sizetemplatemap/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
 
 from sizetemplatemap.models import CreativeSize
 from sizetemplatemap.models import CreativeTemplate
@@ -21,19 +23,19 @@ def custom_404(request):
 
   
 def creative_size_edit(request, creative_size_id):
-    p = get_object_or_404(Poll, pk=poll_id)
+    cs = get_object_or_404(CreativeSize, pk=creative_size_id)
     try:
-        selected_choice = p.choice_set.get(pk=request.POST['choice'])
-    except (KeyError, Choice.DoesNotExist):
-        # Redisplay the poll voting form.
-        return render(request, 'polls/detail.html', {
-            'poll': p,
-            'error_message': "You didn't select a choice.",
+        selected_creative_size = cs.get(pk=request.POST['creative_size_id'])
+    except (KeyError, CreativeTemplate.DoesNotExist):
+        # Redisplay the creative size edit form.
+        return render(request, 'sizetemplatemap/detail.html', {
+            'creative_size_obj': cs,
+            'error_message': "You didn't select a template.",
         })
     else:
-        selected_choice.votes += 1
-        selected_choice.save()
+        selected_creative_size.name = request.POST['cs_name'] 
+        selected_creative_size.save()
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
-        return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))    
+        return HttpResponseRedirect(reverse('sizetemplatemap:results', args=(cs.id,)))    
